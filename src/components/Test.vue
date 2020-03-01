5461b7ab11766a7dad9f2cf543705c0d982316fc
xieningtao <389124248@qq.com>
2020-03-01 21:39:51 +0800

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <test-child v-on:show-me="testClick" v-bind:my-msg=111111></test-child>
+
+        <test-slot>
+            <p>{{slotMsg}}</p>
+        </test-slot>
+
+    </div>
+</template>
+
+<script>
+import TestChild  from './TestChild'
+import TestSlot from './TestSlot'
+export default {
+    data(){
+        return{
+            msg:"hello world",
+            slotMsg:"this is slotMag"
+        }
+    },
+    components:{
+        TestChild,TestSlot
+    },
+    methods:{
+        testClick:function(msg){
+            console.log("click test msg: "+msg)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TestChild.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>{{myMsg}}</p>
+
+        <div v-on:click="clickme">click me</div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        clickme:function(){
+            this.$emit("show-me","this is msg")
+        }
+    },
+    props:{
+        myMsg:{
+            type:Number,
+            default:0
+        },
+
+    }
+}
+</script>
+
+<style>
+
+</style>
